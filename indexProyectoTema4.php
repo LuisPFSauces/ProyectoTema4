e43c14e8dcafea74c54ba0b87c1acbbd3acf70df
Luis Puente <luis.puefer@educa.jcyl.es>
2020-11-04 23:10:50 +0100

    Modificado el ejercico 3 y cambiado el nombre a la carpeta codigoPHP

--- indexProyectoTema4.php
+++ indexProyectoTema4.php
@@ -19,185 +19,43 @@
                 <tr>
                     <th>Numero</th>
                     <th>Descripcion</th>
-                    <th>Mostrar Codigo</th>
-                    <th>Ejercutar</th>
+                    <th>Mostrar Codigo MySQLi</th>
+                    <th>Ejercutar MySQLi</th>
+                    <th>Mostrar Codigo PDO</th>
+                    <th>Ejercutar PDO</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <th>0º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio00.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
+                    <td colspan="4">Scripts iniciales</td>
                     <td><a href="codigoPHP/ejer00.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
                 </tr>
                 <tr>
                     <th>1º</th>
-                    <td>Inicializar variables de los distintos tipos de datos basicos</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio01.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer01.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
+                    <td>Hover</td>
+                    <td><a href="mostrarCodigo/muestraEjercicio101.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
+                    <td><a href="./codigoPHP/ejer101.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
+                    <td><a href="mostrarCodigo/muestraEjercicio201.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
+                    <td><a href="./codigoPHP/ejer201.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>s
                 </tr>
                 <tr>
                     <th>2º</th>
                     <td>Inicializar y mostrar una variable heredoc</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio02.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer02.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
+                    <td><a href="mostrarCodigo/muestraEjercicio102.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
+                    <td><a href="./codigoPHP/ejer102.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
+                    <td><a href="mostrarCodigo/muestraEjercicio202.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
+                    <td><a href="./codigoPHP/ejer202.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
                 </tr>
                 <tr>
                     <th>3º</th>
                     <td>Hora y fecha formateada al castellano</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio03.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer03.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
+                    <td><a href="./mostrarCodigo/muestraEjercicio03.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
+                    <td><a href="./codigoPHP/ejer03.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
+                    <td><a href="mostrarCodigo/muestraEjercicio203.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
+                    <td><a href="./codigoPHP/ejer203.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
                 </tr>
                 <tr>
-                    <th>4º</th>
-                    <td>Fecha y hora formateada al portugués</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio04.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer04.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>5º</th>
-                    <td>Incializar y mostrar variable con marca de tiempo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio05.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer05.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>6º</th>
-                    <td>Fecha y día de la semana dentro de 60 dias</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio06.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer06.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>7º</th>
-                    <td>Mostar el nombre del fichero que se está ejecutando</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio07.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer07.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>8º</th>
-                    <td>Mostrar la IP del equipo desde el que accedes</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio08.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer08.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>9º</th>
-                    <td>Ruta del fichero en ejecución</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio09.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer09.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>10º</th>
-                    <td>Mostrar el contenido del fichero que se está ejecutando</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio10.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer10.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>11º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio11.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer11.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>12º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio12.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer12.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>13º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio13.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer13.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>14º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio14.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer14.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>15º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio15.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer15.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>16º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio16.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer16.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>17º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio17.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer17.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>18º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio18.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer18.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>19º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio19.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer19.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>20º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio20.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer20.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>21º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio21.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer21.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>22º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio22.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer22.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>23º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio23.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer23.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>24º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio24.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer24.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>25º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio25.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer25.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>26º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio26.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer26.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>27º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio27.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer27.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
-                <tr>
-                    <th>28º</th>
-                    <td>Hola Mundo</td>
-                    <td><a href="mostrarCodigo/muestraEjercicio28.php"><img src="webroot/images/ver.png" alt="ver" height="32"></a></td>
-                    <td><a href="codigoPHP/ejer28.php"><img src="webroot/images/ejecutar.png" alt="ejecutar" height="32"></a></td>
-                </tr>
             </tbody>
         </table>
     </main>
